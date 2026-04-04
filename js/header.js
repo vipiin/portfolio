a12af5f6b3bfe46397cc72c6669cd6d29f27d4d9
Vipin Goriparthi <vipin.wipen@hgmail.com>
2026-04-04 16:47:25 +0530

    Refactor headers into a single reusable web component

---
+++ js/header.js
@@ -0,0 +1,28 @@
+class SiteHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <header>
+                <div class="header-container">
+                    <div class="header-side header-left">
+                        <!-- If the page is in a subfolder we might need a different path for img, 
+                             but all files are in the root directory. -->
+                        <img src="img/tux.png" alt="Tux Penguin" width="100">
+                    </div>
+                    <div class="header-content">
+                        <h1>Vipin Goriparthi</h1>
+                        <p class="subtitle">Software Engineer @ Aumovio (ex-Continental)</p>
+                        <nav>
+                            <a href="index.html">Home</a>
+                            <a href="index.html#experience">Experience</a>
+                            <a href="index.html#projects">Projects</a>
+                            <a href="index.html#contact">Contact</a>
+                            <a href="https://drive.google.com/file/d/14y5nMmOFsjow6JfYEG-VFNzeWrq-0Vv0/view?usp=sharing" target="_blank" class="resume-link">Resume</a>
+                        </nav>
+                    </div>
+                </div>
+            </header>
+        `;
+    }
+}
+
+customElements.define('site-header', SiteHeader);
